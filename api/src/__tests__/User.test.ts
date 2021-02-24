b5973854c736c19b0e6bccad80b38a767c290dad
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-24 20:39:02 -0300

    Testes com usuários.

--- api/src/__tests__/User.test.ts
+++ api/src/__tests__/User.test.ts
@@ -1,10 +1,28 @@
 import request from 'supertest'
 import { app } from '../app'
+import createConnection from '../database'
+// foi definido no package.json um comando para remover o banco de dados de teste após relizar o comando "test", "posttest"
+describe('Users', async () => {
+  beforeAll(async () => {
+    const connection = await createConnection()
+    await connection.runMigrations()
+  })
 
-describe('Users', () => {
-  request(app).post('/users') // testando criação de usuários
-  .send({
-    email: 'user@example.com',
-    name: 'User Example'
+  it('Should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({ // testando criação de usuários
+      email: 'user@example.com',
+      name: 'User Example'
+    })
+
+    expect(response.status).toBe(201)
+  })
+
+  it('Should not be able to create a user with existing email', async () => {
+    const response = await request(app).post('/users').send({ // testando criação de usuários
+      email: 'user@example.com',
+      name: 'User Example'
+    })
+
+    expect(response.status).toBe(400)
   })
 })
