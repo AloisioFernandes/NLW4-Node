ab5dd85fa3984dd217e2632258aacb7c64c4d565
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-24 17:09:41 -0300

    Modelo, controller e rota de pesquisas.

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express'
-import { getRepository } from 'typeorm'
-import { User } from '../models/User'
+import { getCustomRepository } from 'typeorm'
+import { UsersRepository } from '../repositories/UsersRepository'
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body
     
-    const usersRepository = getRepository(User) // gerenciador de entidades em um banco de dados
+    const usersRepository = getCustomRepository(UsersRepository) // gerenciador de entidades em um banco de dados
 
     // SELECT * FROM USERS WHERE email = "EMAIL"
     const userAlreadyExists = await usersRepository.findOne({ email })
