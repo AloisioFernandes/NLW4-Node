8da6dd0a5eec839fa82f7549a780d47ea78b0c05
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-25 16:33:37 -0300

    Envio de e-mail para usuário.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ TypeORM
 SQLite(desenvolvimento)
 UUID para ID de usuários
 Jest para testes
+Envio de e-mails com nodeMailer e Ethereal
--- api/src/controllers/SendMailController.ts
+++ api/src/controllers/SendMailController.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
+import SendMailService from "../services/SendMailService";
+import { resolve } from 'path'
 
 class SendMailController {
   async execute(request: Request, response: Response) {
@@ -12,17 +14,17 @@ class SendMailController {
     const surveysRepository = getCustomRepository(SurveysRepository)
     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
 
-    const userAlreadyExists = await usersRepository.findOne({ email })
+    const user = await usersRepository.findOne({ email }) // verifica se usuário existe
 
-    if(!userAlreadyExists) {
+    if(!user) {
       return response.status(400).json({
         error: 'User does not exist!'
       })
     }
 
-    const surveyAlreadyExists = await surveysRepository.findOne({ id: survey_id })
+    const survey = await surveysRepository.findOne({ id: survey_id }) // verifica se pesquisa existe
 
-    if(!surveyAlreadyExists) {
+    if(!survey) {
       return response.status(400).json({
         error: 'Survey does not exist!'
       })
@@ -30,12 +32,23 @@ class SendMailController {
 
     // Salvar informações na tabela
     const surveyUser = surveysUsersRepository.create({
-      user_id: userAlreadyExists.id,
+      user_id: user.id,
       survey_id
     })
-    await surveysUsersRepository.save(surveyUser)
 
+    await surveysUsersRepository.save(surveyUser)
+    
     // Enviar e-mail
+    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs')
+
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description
+    }
+
+    await SendMailService.execute(email, survey.title, variables, npsPath)
+    
     return response.json(surveyUser)
   }
 }
