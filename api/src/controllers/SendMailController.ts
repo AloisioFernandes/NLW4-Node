ab4f6ef1f07235b910018cac9d127e535e1128c6
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-25 18:15:52 -0300

    Template de e-mail e relação um para muitos.

--- api/src/controllers/SendMailController.ts
+++ api/src/controllers/SendMailController.ts
@@ -9,25 +9,45 @@ import { resolve } from 'path'
 class SendMailController {
   async execute(request: Request, response: Response) {
     const { email, survey_id } = request.body
-
+    
     const usersRepository = getCustomRepository(UsersRepository)
     const surveysRepository = getCustomRepository(SurveysRepository)
     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
-
+    
     const user = await usersRepository.findOne({ email }) // verifica se usuário existe
-
+    
     if(!user) {
       return response.status(400).json({
         error: 'User does not exist!'
       })
     }
-
+        
     const survey = await surveysRepository.findOne({ id: survey_id }) // verifica se pesquisa existe
-
+    
     if(!survey) {
       return response.status(400).json({
         error: 'Survey does not exist!'
       })
+    }
+    
+    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs')
+
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description,
+      user_id: user.id,
+      link: process.env.URL_MAIL
+    }
+
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: [{user_id: user.id}, {value: null}],
+      relations: ['user', 'survey'] // relacionamento um para muitos, retornará o objeto completo como resposta da requisição
+    })
+
+    if(surveyUserAlreadyExists) { // impede que vários registros Pesquisa Usuário sejam criados na tabela
+      await SendMailService.execute(email, survey.title, variables, npsPath)
+      return response.json(surveyUserAlreadyExists)
     }
 
     // Salvar informações na tabela
@@ -39,13 +59,6 @@ class SendMailController {
     await surveysUsersRepository.save(surveyUser)
     
     // Enviar e-mail
-    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs')
-
-    const variables = {
-      name: user.name,
-      title: survey.title,
-      description: survey.description
-    }
 
     await SendMailService.execute(email, survey.title, variables, npsPath)
     
